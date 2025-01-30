223704b5731e0df51001b8d40f391ec8bbecbc86
muneefa <muneefahanan@gmail.com>
2025-01-30 12:32:55 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+//**-component directive */
+export class AppComponent {
+  title = 'test';
+
+  ngOnInit(): void {
+    console.log("hi");
+
+  }
+
+
+  //**-structural directive */
+  // if
+  isAllowed:boolean =true;
+  // for loop
+  userList:any[]=[
+    {name:'sara', age:21},
+    {name:'sana', age:26},
+    {name:'raj', age:31},
+  ];
+  // switch
+  selectedValue:string="";
+  // **-atttribute directive */
+  // ngstyle 
+  paracolor:string ='green'
+  //ngclass
+  isActive:boolean=true;
+
+  
+}
